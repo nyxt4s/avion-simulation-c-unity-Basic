07a45be1ef9007688ecd8d34025d1a21b2e397a9
RODERICK LUIS MEDINA GARAY <RODERICK.MEDINA@correoaiep.cl>
2021-05-25 17:32:10 -0400

    Desarrollo de movimientos basicos en unity con un simulador de avion

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private Variables
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this  is where are get player imput
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //whe move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //whe turn the veehicle     
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
